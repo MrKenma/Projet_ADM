fa2e5a87d4d7ff7d5822e3163d25ad00dbf5c14f
MrKenma <etu47233@henallux.be>
2022-12-03 09:38:54 +0100

    c'est bon comme ça ?

--- ProjetMath/ProjetMath/Program.cs
+++ ProjetMath/ProjetMath/Program.cs
@@ -5,6 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjetMath {
+    class Couts {
+        public int ClientsPrioritaires = 0;
+        public int ClientsOrdinaires = 0;
+        public int StationsOrdinairesOccupées = 0;
+        public int StationsPrioritairesOccupées = 0;
+        public int StationsInoccupées = 0;
+        public int PrioritairesDéchus = 0;
+
+        public Couts() { }
+    }
     class Program {
         // Générer une suite de nombres pseudo-aléatoires à partir de valeurs de X0, a, c et m
         // Vérifier 
@@ -69,8 +79,7 @@ namespace ProjetMath {
 
         static bool VerificationValeurs(int X0, int a, int c, int m) {
             // m : de préférence une valeur élevée
-            // c : si c = 0, calculs rapides mais période courte => à éviter
-            // a : rejeter a = 0 et a = 2, a devrait être au moins plus grand que 2
+            
             if (m <= 0) {
                 Console.WriteLine("m > 0 : KO");
                 return false;
@@ -84,9 +93,11 @@ namespace ProjetMath {
                 Console.WriteLine("0 <= X0 < m : KO");
                 return false;
             } else {
+                // c : si c = 0, calculs rapides mais période courte => à éviter
                 if (c == 0) {
                     Console.WriteLine("ATTENTION : c = 0 -> les calculs seront rapides mais la période courte");
                 }
+                // a : rejeter a = 0 et a = 1, a devrait être au moins plus grand que 2
                 if (a < 2) {
                     Console.WriteLine("ATTENTION : a est plus petit que 2 (à rejeter)");
                 }
@@ -94,11 +105,32 @@ namespace ProjetMath {
             }
         }
 
+        static void TestDesCourses(int[] nombres) {
+            // 6 étapes :
+            // Etape 1 : poser une hypothèse H0 (suite générée acceptable ou non)
+            // Etape 2 : fixer le niveau d'incertitude alpha (en général alpha = 5%)
+            // Etape 3 : tableau recensé des fréquences observées et des fréquences théoriques + calcul de la statistique observable X2v
+            // Etape 4 : vérifier les contraintes à respecter pour chaque test et, le cas échéant, retourner à l'étape 3 en effectuant les regroupements
+            // Etape 5 : établir la zone de non-rejet en fonction du nombre de degrés de liberté
+            // Etape 6 : prendre une décision, rejet ou non-rejet de l'hypothèse
+        }
+
+        // Partie 2 : implémentation du DA
+        static int NbStationsOptimal(int nbStationsMin, int nbStationsMax, int tempsSimulation, int X0, int a, int c, int m) {
+            Couts[] couts = new Couts[nbStationsMax - nbStationsMin + 1];
+
+            for (int nbStations = nbStationsMin; nbStations <= nbStationsMax; nbStations++) {
+
+            }
+
+            return 1;
+        }
+
         static void Main(string[] args) {
-            int X0 = 19;
-            int a = 22;
-            int c = 4;
-            int m = 63;
+            int X0 = 19;  
+            int a = 261;
+            int c = 7;
+            int m = 13000;
             bool nombresValides;
 
             nombresValides = VerificationValeurs(X0, a, c, m);
@@ -106,8 +138,20 @@ namespace ProjetMath {
                 Console.WriteLine("Les nombres ne sont pas valides");
             } else {
                 Console.WriteLine("Les nombres sont valides");
+                int[] nombresAleatoires = new int[m];
+                int X1;
+
+                // éviter de placer les valeurs dans un tableau, ça va utiliser beaucoup de place mémoire
+                nombresAleatoires[0] = X0;
+                for (int i = 1; i < m; i++) {
+                    X1 = (a * X0 + c) % m;
+                    nombresAleatoires[i] = X1;
+                    X0 = X1;
+                }
+                // Vérifier si la suite est assez longue pour nos tests => Voir DA
+                // Si non : 600 * (1 + 9 + 1 + (9 * 1 + 1)) = 600 * 21 = 12600 
+                TestDesCourses(nombresAleatoires);
             }
-            
             Console.ReadLine();
         }
     }
